73d5207e5d61c26dd8fcd1199007c8b56668e5c5
Ana Carla <acarlagomes250@gmail.com>
2020-02-02 23:28:58 -0300

    Repository

---
+++ Demo.Domain/Interfaces/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Demo.Domain.Interfaces.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Add(TEntity entity);
+        void Update(TEntity entity);
+        IEnumerable<TEntity> GetAll();
+        TEntity GetForId(int id);
+        IEnumerable<TEntity> Search(Expression<Func<TEntity,bool>> predicado);
+        void Remove(TEntity entity);
+    }
+}
